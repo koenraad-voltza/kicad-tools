cc76f590a87f17eebcb3b4021fc606f2ba4aad0a
Koenraad Van den Eeckhout <koenraad@voltza.be>
2019-12-10 11:11:22 +0100

    added schematic export

--- schematic_variant.py
+++ schematic_variant.py
@@ -5,9 +5,11 @@ from libs.sch import *
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Update Kicad schematic variant schematic')
     parser.add_argument('input', action="store", help='Path containing input .sch file')
+    parser.add_argument('name', action="store", help='File name of main sch file')
     parser.add_argument('variant', action="store", help='Component field name of Variant')
     args = parser.parse_args()
 
+    ## TODO check if variant exists
     ## Copy all files to variant folder
     variantpath = args.input+'/'+args.variant+'/'
     os.mkdir(variantpath)
@@ -29,3 +31,10 @@ if __name__ == '__main__':
                             print("Setting component "+ component.fields[0]['ref'] + " to DNP")
                             component.fields[1]['ref']="\"DNP\""
             sch.save()
+    ## Generate schematic
+    ##TODO BOM?
+    name = args.name
+    os.system("python -m eeschema.schematic export -f pdf -a " + variantpath + name+".sch " + args.input+'/'+outputs)
+    os.system("rm " + args.input+'/'+outputs + "/export_schematic_screencast.ogv")
+    os.system("mv "+args.input+'/'+outputs +'/'+name + ".pdf "args.input+'/'+outputs +'/'+name+"_"+args.variant+".pdf")
+    os.system("rm -r " + variantpath)
